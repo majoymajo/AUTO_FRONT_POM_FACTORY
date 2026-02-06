c37ec902b26884f6ad1bffd25a3cca12807da43a
ChristopherPalloArias <cipallo@uce.edu.ec>
2026-02-06 14:33:28 -0500

    fix: fix configuration AWS

--- aws/main.tf
+++ aws/main.tf
@@ -1,9 +1,23 @@
+resource "tls_private_key" "ec2_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
+resource "aws_key_pair" "generated_key" {
+  key_name   = "${var.instance_name}-key"
+  public_key = tls_private_key.ec2_key.public_key_openssh
+}
+
+resource "local_sensitive_file" "private_key" {
+  content  = tls_private_key.ec2_key.private_key_pem
+  filename = "${path.module}/${var.instance_name}-key.pem"
+}
 
 resource "aws_security_group" "web_sg" {
-  name        = "allow_web_traffic"
-  description = "Allow HTTP and SSH inbound traffic"
+  name        = "sofkianos-sg"
+  description = "Allow web, api, rabbitmq and ssh traffic"
 
+  # SSH
   ingress {
     from_port   = 22
     to_port     = 22
@@ -11,6 +25,55 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Frontend
+  ingress {
+    from_port   = 5173
+    to_port     = 5173
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Producer API
+  ingress {
+    from_port   = 8082
+    to_port     = 8082
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Consumer
+  ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # RabbitMQ AMQP
+  ingress {
+    from_port   = 5672
+    to_port     = 5672
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # RabbitMQ UI
+  ingress {
+    from_port   = 15672
+    to_port     = 15672
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Dozzle
+  ingress {
+    from_port   = 8888
+    to_port     = 8888
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP
   ingress {
     from_port   = 80
     to_port     = 80
@@ -18,6 +81,7 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Egress
   egress {
     from_port   = 0
     to_port     = 0
@@ -26,9 +90,11 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
+
 resource "aws_instance" "web_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  key_name      = aws_key_pair.generated_key.key_name
 
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   user_data              = templatefile("${path.module}/user_data.sh", {
@@ -43,4 +109,9 @@ resource "aws_instance" "web_server" {
 
 output "instance_public_ip" {
   value = aws_instance.web_server.public_ip
+}
+
+output "private_key" {
+  value     = tls_private_key.ec2_key.private_key_pem
+  sensitive = true
 }
