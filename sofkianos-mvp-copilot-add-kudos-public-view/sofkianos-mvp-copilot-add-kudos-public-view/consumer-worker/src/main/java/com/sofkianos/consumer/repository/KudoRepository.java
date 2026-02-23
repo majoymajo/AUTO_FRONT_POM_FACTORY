fc467d59bcc0707765485e2f3cb22b172df49630
Maria Jose Castro Pavon <150195540+majoymajo@users.noreply.github.com>
2026-02-23 11:20:54 -0500

    Delete sofkianos-mvp-copilot-add-kudos-public-view/sofkianos-mvp-copilot-add-kudos-public-view directory

--- sofkianos-mvp-copilot-add-kudos-public-view/sofkianos-mvp-copilot-add-kudos-public-view/consumer-worker/src/main/java/com/sofkianos/consumer/repository/KudoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sofkianos.consumer.repository;
-
-import com.sofkianos.consumer.entity.Kudo;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface KudoRepository extends CrudRepository<Kudo, Long> {
-}
