e7d537eeab01611e49b5bb9a3ea6eafabe384856
Elizabeth Tamayo <dianelizbty@gmail.com>
2026-02-27 01:40:18 -0500

    chore: add Testcontainers dependencies for PostgreSQL integration tests

--- consumer-worker/src/test/java/com/sofkianos/consumer/controller/KudosControllerIntegrationTest.java
+++ consumer-worker/src/test/java/com/sofkianos/consumer/controller/KudosControllerIntegrationTest.java
@@ -8,10 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDateTime;
@@ -26,9 +24,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 class KudosControllerIntegrationTest {
-
-    @Container
-    @ServiceConnection
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
 
     @Autowired
