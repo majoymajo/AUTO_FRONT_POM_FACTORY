5320fd62bd7f34839d870887b2f4f9fa5f891351
Majo Castro <majo_castro21@hotmail.com>
2026-03-13 16:55:28 -0500

    🛠️ refactor: remove unused imports from KudosSendSteps.java

--- src/test/java/com/sofka/automation/stepdefinitions/KudosSendSteps.java
+++ src/test/java/com/sofka/automation/stepdefinitions/KudosSendSteps.java
@@ -2,8 +2,6 @@ package com.sofka.automation.stepdefinitions;
 
 import com.sofka.automation.drivers.DriverFactory;
 import com.sofka.automation.pages.KudoSendPage;
-import com.sofka.automation.pages.KudosListPage;
-import com.sofka.automation.pages.LandingPage;
 import com.sofka.automation.pages.NavigationBar;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
